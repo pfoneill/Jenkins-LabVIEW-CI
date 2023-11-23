0e248e1c0a55dd479850eb7ff65a068c47f2a263
Paul O'Neill <paul.oneill@abcellera.com>
2023-11-22 18:53:45 -0800

    Add boilerplate code

--- src/com/labviewbuilder/labviewbuilder.groovy
+++ src/com/labviewbuilder/labviewbuilder.groovy
@@ -1,1 +1,16 @@
-// labview builder
+#!/usr/bin/env groovy
+package com.labviewbuilder
+
+class LabVIEWBuilder implements Serializable {
+
+    def steps
+
+    def setAppName() {
+    // set application name
+    }
+
+    def build() {
+        // build the application
+    }
+
+}
